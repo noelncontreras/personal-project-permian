277191cd33c66b4065e8f7e65a62efd2473521ac
Noel N. Contreras <noeln.contreras@Noels-MacBook-Air.local>
2019-09-25 17:44:40 -0500

    need to work on add service functionality

--- src/Components/AddService.js
+++ src/Components/AddService.js
@@ -1,11 +1,23 @@
 import React, {Component} from "react";
 
 export default class AddService extends Component {
-    
+    constructor() {
+        super();
+        this.state = {
+            service_description: ""
+        };
+    };
+
+
     render() {
         return (
             <div>
-                <h1>Add Service</h1>
+                <h1>Add a Service</h1>
+                <label>Category:</label>
+                <select>
+                    <option value="test1">HotShot</option>
+                    <option value="test2">Graphic</option>
+                </select>
             </div>
         )
     }
